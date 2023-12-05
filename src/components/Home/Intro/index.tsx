cb64f0f162f8c6cefe304edfe66170921f578c2a
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2023-12-05 15:46:34 +0100

    display count of invoices

--- src/components/Home/Intro/index.tsx
+++ src/components/Home/Intro/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import { useAppSelector } from '../../../state/hooks'
 
 const Intro = () => {
+    const invoice = useAppSelector((state) => state.invoice)
+
     return (
         <div>
             <div>
                 <h1>Invoices</h1>
-                <p>There are X total invoices</p>
+                <p>There are {invoice.invoices.length} total invoices</p>
             </div>
             <p>Filter by status</p>
             <button>New Invoice</button>
