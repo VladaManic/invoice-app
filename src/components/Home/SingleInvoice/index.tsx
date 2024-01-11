a5b783cd06c92a83be3898173afabb3a70530dc0
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2024-01-11 10:29:03 +0100

    small style corrections

--- src/components/Home/SingleInvoice/index.tsx
+++ src/components/Home/SingleInvoice/index.tsx
@@ -17,26 +17,26 @@ const SingleInvoice = ({ invoice }: Props) => {
     return (
         <NavLink
             to={`/invoice/${invoice.id}`}
-            className="xs:w-[290px] block sm:w-[327px] md:w-[672px] lg:w-[730px]"
+            className="block xs:w-[290px] sm:w-[327px] md:w-[672px] lg:w-[730px]"
         >
-            <button className="xs:flex-col mb-4 flex w-full items-center rounded-md bg-defaultWhite p-4 shadow-[0_10px_10px_-10px_rgba(0,0,0,0.1)] md:flex-row md:pl-8">
-                <div className="xs:mb-[20px] xs:w-full flex max-md:justify-between md:mb-0 md:w-1/3">
+            <button className="mb-4 flex w-full items-center rounded-md bg-defaultWhite p-4 shadow-[0_10px_10px_-10px_rgba(0,0,0,0.1)] xs:flex-col md:flex-row md:pl-8">
+                <div className="flex max-md:justify-between xs:mb-[20px] xs:w-full md:mb-0 md:w-1/3">
                     <p className="w-28 text-left text-xs text-defaultText">
                         #
                         <span className="font-spartanBold text-defaultBlack">
                             {invoice.id}
                         </span>
                     </p>
-                    <p className="xs:text-right w-40 text-xs md:text-left">
+                    <p className="w-40 text-xs xs:text-right md:text-left">
                         <span className="text-defaultText">Due&nbsp;</span>
                         <span className="text-defaultText">
                             {format(date, 'dd MMM y')}
                         </span>
                     </p>
                 </div>
-                <div className="xs:w-full flex items-center justify-between md:w-2/3">
-                    <div className="xs:flex-col flex items-center justify-between md:w-3/5 md:flex-row">
-                        <p className="xs:mb-[5px] text-left text-xs text-defaultText md:mb-0">
+                <div className="flex items-center justify-between xs:w-full md:w-2/3">
+                    <div className="flex justify-between xs:flex-col md:w-3/5 md:flex-row md:items-center">
+                        <p className="text-left text-xs text-defaultText xs:mb-[5px] md:mb-0 md:ml-[30px]">
                             {invoice.clientName}
                         </p>
                         <p className="flex font-spartanBold text-base text-defaultBlack">
