98b2a1c7fd58722e161def3ef63f2ef785a34a41
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2024-01-12 14:41:17 +0100

    if form modal opened, aside fixed

--- src/layout/Aside/index.tsx
+++ src/layout/Aside/index.tsx
@@ -1,10 +1,20 @@
+import { useAppSelector } from '../../state/hooks'
+import clsx from 'clsx'
+
 import packmanIcon from '../../assets/img/packman.svg'
 import moonIcon from '../../assets/img/moon.svg'
 import personImg from '../../assets/img/person.png'
 
 const Aside = () => {
+    const invoiceRedux = useAppSelector((state) => state.invoice)
+
     return (
-        <div className="left-0 top-0 z-[101] flex justify-between overflow-hidden bg-asideBg xs:absolute xs:w-full lg:fixed lg:h-screen lg:w-[96px] lg:flex-col lg:rounded-r-2xl">
+        <div
+            className={clsx(
+                'left-0 top-0 z-[101] flex justify-between overflow-hidden bg-asideBg xs:w-full lg:fixed lg:h-screen lg:w-[96px] lg:flex-col lg:rounded-r-2xl',
+                invoiceRedux.openFormModal ? 'xs:fixed' : 'xs:absolute'
+            )}
+        >
             <div className="relative overflow-hidden rounded-br-2xl rounded-tr-2xl bg-packmanUp xs:h-[72px] xs:w-[72px] md:h-[80px] md:w-[80px] lg:h-24 lg:w-24">
                 <div className="h-1/2"></div>
                 <div className="h-1/2 rounded-tl-2xl bg-packmanDown"></div>
