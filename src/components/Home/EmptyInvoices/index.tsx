56e5535bb5219755298a51f4ff74dc08303de79e
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2024-01-16 16:27:58 +0100

    empty home theme switch

--- src/components/Home/EmptyInvoices/index.tsx
+++ src/components/Home/EmptyInvoices/index.tsx
@@ -1,9 +1,11 @@
-import { useAppDispatch } from '../../../state/hooks'
+import { useAppDispatch, useAppSelector } from '../../../state/hooks'
 import { setOpenModal } from '../../../state/invoice/invoiceSlice'
+import clsx from 'clsx'
 
 import emptyIcon from '../../../assets/img/empty.png'
 
 const EmptyInvoices = () => {
+    const invoiceRedux = useAppSelector((state) => state.invoice)
     const dispatch = useAppDispatch()
 
     //On click New Invoice btn, open form modal
@@ -20,14 +22,24 @@ const EmptyInvoices = () => {
                     className="ml-auto mr-auto"
                 />
             </div>
-            <h1 className="mb-[30px] text-[20px] leading-[22px] text-defaultBlack">
+            <h1
+                className={clsx(
+                    'mb-[30px] text-[20px] leading-[22px]',
+                    invoiceRedux.colorTheme === 'light'
+                        ? 'text-defaultBlack'
+                        : 'text-defaultWhite'
+                )}
+            >
                 There is nothing here
             </h1>
             <p className="ml-auto mr-auto w-[230px] text-xs">
                 {' '}
                 Create an invoice by clicking the{' '}
                 <button
-                    className="p-0 font-spartanBold hover:border-transparent focus:outline-none focus:ring-0"
+                    className={clsx(
+                        'p-0 font-spartanBold hover:border-transparent focus:outline-none focus:ring-0',
+                        invoiceRedux.colorTheme === 'dark' && 'bg-themeDark'
+                    )}
                     onClick={onClickHandler}
                 >
                     New Invoice
