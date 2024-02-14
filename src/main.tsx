9ccfc8940202a8a45b77cd7ee9cc3b36960ce61f
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2024-02-14 11:43:03 +0100

    react query devtool added

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './state/store.ts'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 // Create a client
 const queryClient = new QueryClient({
@@ -23,6 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             <Provider store={store}>
                 <QueryClientProvider client={queryClient}>
                     <App />
+                    <ReactQueryDevtools initialIsOpen={false} />
                 </QueryClientProvider>
             </Provider>
         </BrowserRouter>
