bb2ff46751a52d7b0e4de6d9682e0d19282bea5b
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2024-02-22 15:31:43 +0100

    test for util function which calculates total

--- src/utils/calculateTotal.ts
+++ src/utils/calculateTotal.ts
@@ -1,6 +1,6 @@
 import { InvoiceObj, ItemObj } from '../types/interfaces'
 
-const calculateTotal = (invoice: InvoiceObj) => {
+const calculateTotal = (invoice: InvoiceObj | { items: never[] }) => {
     let result = 0
     invoice.items.forEach((item: ItemObj) => {
         result = result + item.quantity * item.price
